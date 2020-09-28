0e8c1a755fa8174c46c0c280435fc6dac226399b
vagrant <vagrant@localhost.localdomain>
2020-09-28 07:03:47 +0000

    [Add]webpacker

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
 
-  devise_for :admins ,controllers: { sessions: 'public/admins/sessions',  
-                                      registrations: 'public/admins/passwords',
-                                      passwords: 'public/admins/passwords' }
-  devise_for :users ,controllers: {   sessions: 'public/users/sessions',
-                                      registrations: 'public/users/registrations', 
-                                      passwords: 'public/users/passwords' }
-
-  }
+  devise_for :admins
+  devise_for :users
   namespace :admin do
     get 'home/top'
   end
   namespace :public do
     get 'home/top'
-    get 'home/about' 
+    get 'home/about'
   end
   root 'public/home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
