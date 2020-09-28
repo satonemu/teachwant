907a09e41e46fdb1871e010ad70fe0b0b140a274
Emiko <emi9361@hotmail.co.jp>
2020-09-28 10:08:10 +0900

    admin devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
+  devise_for :admins
   devise_for :users
+  
+  namespace :admin do
+    get 'home/top'
+  end
+  namespace :public do
+    get 'home/top'
+    get 'home/about' 
+  end
+  root 'public/home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
